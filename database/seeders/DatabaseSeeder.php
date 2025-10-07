08030b5c576e9bc5b3f23753463c6164792fad67
walaaRehawi <walaaalrehawi@gmail.com>
2025-10-07 15:50:02 +0300

    v8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,23 +22,30 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
       
-        // $this->call([
-        //     RolePermissionSeeder::class
-        // ]);
+        $this->call([
+            RolePermissionSeeder::class
+        ]);
 
         // $this->call([
         //     StudentSeeder::class,
         // ]);
-        $this->call([
-            SettingsTableSeeder::class,
-        ]);
+        // $this->call([
+        //     SettingsTableSeeder::class,
+        // ]);
           $user1 = User::create([
-            'name' => 'Manager One',
-            'email' => 'manager1@example.com',
+            'name' => 'student One',
+            'email' => '123456',
             'password' => Hash::make('11111111'),
         ]);
 
-        $user1->assignRole('manger');
+        $user1->assignRole('student');
+          $user3 = User::create([
+            'name' => 'student Two',
+            'email' => '123455',
+            'password' => Hash::make('11111111'),
+        ]);
+
+        $user3->assignRole('student');
 
         $user2 = User::create([
             'name' => 'Manager Two',
@@ -47,6 +54,22 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $user2->assignRole('manger');
+       
+        $user4 = User::create([
+            'name' => 'teacher One',
+            'email' => 'teacher1@example.com',
+            'password' => Hash::make('11111111'),
+        ]);
+
+        $user4->assignRole('teacher');
+       
+        $user5 = User::create([
+            'name' => 'superVisorTeacher One',
+            'email' => 'superVisorTeacher1@example.com',
+            'password' => Hash::make('11111111'),
+        ]);
+
+        $user5->assignRole('superVisorTeacher');
        
             // $this->call([
             //     StudentSeeder::class,
