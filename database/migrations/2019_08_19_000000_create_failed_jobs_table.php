5ac27b2cb328003eb5228de6fc655fb3987be3df
walaaRehawi <walaaalrehawi@gmail.com>
2025-11-14 13:40:21 +0300

    v55

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid',191)->unique();
+            $table->string('uuid',191);
             $table->text('connection',191);
             $table->text('queue' ,191);
             $table->longText('payload',191);
