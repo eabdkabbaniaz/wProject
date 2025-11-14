714f124770cad0b63a36269585abb20197f83b7f
walaaRehawi <walaaalrehawi@gmail.com>
2025-11-14 13:34:28 +0300

    v54

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email',191)->primary();
-            $table->string('token' ,191);
+            $table->string('email',150)->primary();
+            $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
